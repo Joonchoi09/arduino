2802101331b69a93866a426a5f7c80b4036c4bd1
Joonchoi09 <72638509+Joonchoi09@users.noreply.github.com>
2020-10-17 11:59:07 +0900

    Add files via upload

---
+++ 19-09-03LED/19-09-03LED.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(9,OUTPUT);  //R
+  pinMode(10,OUTPUT); //G
+  pinMode(11,OUTPUT); //B
+  
+}
+
+void loop() {
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
+  
+
+}
